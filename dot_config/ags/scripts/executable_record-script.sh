9c23b5822c511c7449c59c4a505cddbd9ec5f056
CheaVuthearith <cheavuthearith0@gmail.com>
2025-03-29 23:30:00 +0700

    fix: copy to clipboard after recording end

--- dot_config/ags/scripts/executable_record-script.sh
+++ dot_config/ags/scripts/executable_record-script.sh
@@ -14,26 +14,29 @@ mkdir -p "$(xdg-user-dir VIDEOS)"
 cd "$(xdg-user-dir VIDEOS)" || exit
 if pgrep wf-recorder >/dev/null; then
   notify-send "Recording Stopped" "Stopped" -a 'record-script.sh' &
-  pkill wf-recorder &
+  NAME="$(ps ax | grep 'wf-recorder' | head -n 1 | awk -F'wf-recorder:' '{print $2}' | awk '{print $1}')"
+  wl-copy --type text/uri-list "$NAME"
+
+  pkill wf-recorder
+
 else
   notify-send "Starting recording" 'recording_'"$(getdate)"'.mp4' -a 'record-script.sh'
   FILE='/recording_'"$(getdate)"'.mp4'
   WORKSPACES="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
   WINDOWS="$(hyprctl clients -j | jq -r --argjson workspaces "$WORKSPACES" 'map(select([.workspace.id] | inside($workspaces)))')"
-  GEOM=$(echo "$WINDOWS" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | slurp $SLURP_ARGS)
+  GEOM=$(echo "$WINDOWS" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | slurp "$SLURP_ARGS")
 
   if [[ "$1" == "--sound" ]]; then
-    wf-recorder --pixel-format yuv420p -f ".$FILE" -t --geometry "$GEOM" --audio="$(getaudiooutput)" &
+    exec -a "wf-recorder:file://$PWD$FILE" wf-recorder --pixel-format yuv420p -f ".$FILE" -t --geometry "$GEOM" --audio="$(getaudiooutput)" &
     disown
   elif [[ "$1" == "--fullscreen-sound" ]]; then
-    wf-recorder -o $(getactivemonitor) --pixel-format yuv420p -f ".$FILE" -t --audio="$(getaudiooutput)" &
+    exec -a "wf-recorder:file://$PWD$FILE" wf-recorder -o "$(getactivemonitor)" --pixel-format yuv420p -f ".$FILE" -t --audio="$(getaudiooutput)" &
     disown
   elif [[ "$1" == "--fullscreen" ]]; then
-    wf-recorder -o $(getactivemonitor) --pixel-format yuv420p -f ".$FILE" -t &
+    exec -a "wf-recorder:file://$PWD$FILE" wf-recorder -o "$(getactivemonitor)" --pixel-format yuv420p -f ".$FILE" -t &
     disown
   else
-    wf-recorder --pixel-format yuv420p -f ".$FILE" -t --geometry "$GEOM" &
+    exec -a "wf-recorder:file://$PWD$FILE" wf-recorder --pixel-format yuv420p -f ".$FILE" -t --geometry "$GEOM" &
     disown
   fi
-  wl-copy --type text/uri-list "file://$PWD$FILE"
 fi
