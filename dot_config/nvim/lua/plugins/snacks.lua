015866f133d33bfa2187e3a0ba44b3ab3a579f70
CheaVuthearith <cheavuthearith0@gmail.com>
2025-04-02 21:06:49 +0700

    style: random dashboard img

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,4 @@
+math.randomseed(os.time() + vim.loop.hrtime())
 return {
   "folke/snacks.nvim",
   ---@type snacks.Config
@@ -6,7 +7,9 @@ return {
       sections = {
         {
           section = "terminal",
-          cmd = "chafa ~/.config/nvim/pngs/wall2.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
+          cmd = "chafa ~/.config/nvim/pngs/wall"
+            .. math.random(3)
+            .. ".png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
           height = 17,
           padding = 1,
         },
