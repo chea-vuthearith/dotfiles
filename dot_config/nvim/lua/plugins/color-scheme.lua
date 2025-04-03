da9c6ca3e216836c93b6e9c13f0d0ef811be0f39
CheaVuthearith <cheavuthearith0@gmail.com>
2025-04-03 18:11:52 +0700

    styling: move cmdling to the bottom

--- dot_config/nvim/lua/plugins/color-scheme.lua
+++ dot_config/nvim/lua/plugins/color-scheme.lua
@@ -1,6 +1,17 @@
 return {
   { "catppuccin", opts = { transparent_background = true } },
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
+  {
+    "folke/noice.nvim",
+    config = {
+      cmdline = {
+        view = "cmdline",
+      },
+      presets = {
+        command_palette = false, -- position the cmdline and popupmenu together
+      },
+    },
+  },
 }
 
 -- return {
