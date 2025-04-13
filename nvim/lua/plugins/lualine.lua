9b2b3367236c2c8cdd87823fc11ead8b330cd980
chea-vuthearith <cheavuthearith0@gmail.com>
2025-04-13 21:52:51 +0700

    styling: adjust lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,9 +3,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
       sections = {
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = { "filetype" },
+        lualine_b = { "branch" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
