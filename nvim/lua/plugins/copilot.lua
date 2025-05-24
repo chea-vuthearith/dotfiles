1d5eaf79be1dc4fbadef6ed5d3351ab9e24a1256
chea-vuthearith <cheavuthearith0@gmail.com>
2025-05-24 12:46:41 +0700

    fix: copilot nonsense

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,1 +1,18 @@
-return { "github/copilot.vim" }
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+        enabled = true,
+        auto_trigger = true,
+      },
+    },
+  },
+  { "zbirenbaum/copilot-cmp", enabled = false },
+}
