c5dda4123275d383b5fee02a37b6e3cf97de2abc
chea-vuthearith <cheavuthearith0@gmail.com>
2025-06-08 19:34:51 +0700

    fix: disable shutdown schedule
    it doesn't work

--- nix/home.nix
+++ nix/home.nix
@@ -335,8 +335,8 @@
       settings = {
         general = {
           lock_cmd = "pidof hyprlock || hyprlock";
-          before_sleep_cmd = "shutdown -h +20; loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on; shutdown -c";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
         };
 
         listener = [
