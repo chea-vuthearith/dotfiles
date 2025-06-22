36132c5ed08dac77ea4fbeb36a4a2bbb8b3937b1
chea-vuthearith <cheavuthearith0@gmail.com>
2025-06-23 00:17:30 +0700

    fix: configure blink cmp

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,9 +1,37 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_deply_ms = 500,
+      },
+      list = {
+        selection = {
+          preselect = function(ctx)
+            return not require("blink.cmp").snippet_active({ direction = 1 })
+          end,
+        },
+      },
+      ghost_text = { enabled = false },
+      accept = {
+        auto_brackets = { enabled = false },
+      },
+    },
+
     keymap = {
-      ["<Tab>"] = { "select_and_accept" },
-      ["<CR>"] = {},
+      preset = "super-tab",
+      ["S-Tab"] = {},
+      ["<C-space>"] = {
+        ---@param cmp blink.cmp.API
+        function(cmp)
+          if cmp.is_visible() then
+            return cmp.hide()
+          else
+            return cmp.show()
+          end
+        end,
+      },
     },
   },
 }
