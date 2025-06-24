de11742f4076180925371ac1f5e4641d9ff12a94
chea-vuthearith <cheavuthearith0@gmail.com>
2025-06-24 20:36:43 +0700

    fix: absolute imports for vtsls

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,6 +4,11 @@ return {
   opts = {
     inlay_hints = { enabled = false },
     servers = {
+      vtsls = {
+        typescript = {
+          importModuleSpecifier = "non-relative",
+        },
+      },
       biome = {
         cmd = { "biome", "lsp-proxy", "--config-path", vim.env.BIOME_CONFIG_PATH },
         root_dir = vim.fn.getcwd(),
