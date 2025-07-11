7bb1f61518c72799efa345a193feaf177da4287b
chea-vuthearith <cheavuthearith0@gmail.com>
2025-07-11 18:59:00 +0700

    fix: blink crashing with preset

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -17,6 +17,11 @@ return {
 
     keymap = {
       preset = "super-tab",
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
       ["S-Tab"] = {},
       ["<C-space>"] = {
         ---@param cmp blink.cmp.API
