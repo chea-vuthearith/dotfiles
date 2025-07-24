af5351cb9a418f73dcc85b96e12f2d16c9172bc7
chea-vuthearith <cheavuthearith0@gmail.com>
2025-07-25 00:02:10 +0700

    chore: remove pngs

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -6,15 +6,7 @@ return {
     dashboard = {
       sections = {
         {
-          section = "terminal",
-          cmd = "chafa ~/.config/nvim/pngs/wall"
-            .. math.random(3)
-            .. ".png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
-          height = 17,
-          padding = 1,
-        },
-        {
-          pane = 2,
+          pane = 1,
           { section = "keys", gap = 1, padding = 1 },
           { section = "startup" },
         },
