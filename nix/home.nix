5f3125da437f309d8336d96b39255974f9b4e490
chea-vuthearith <cheavuthearith0@gmail.com>
2025-07-30 19:06:52 +0700

    feat: reload hyprland on wake

--- nix/home.nix
+++ nix/home.nix
@@ -357,7 +357,7 @@
           {
             timeout = 3 * 60;
             on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
+            on-resume = "hyprctl dispatch dpms on; hyprctl reload";
           }
           {
             timeout = 5 * 60;
