8562af86d6e40e1b9c0797b5bf1ab79f6b4a1f5b
chea-vuthearith <cheavuthearith0@gmail.com>
2025-08-09 18:37:20 +0700

    feat: hide text in ly

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -48,7 +48,12 @@
       enable = true;
       ly = {
         enable = true;
-        settings = { hide_borders = true; };
+        settings = {
+          text_in_center = true;
+          hide_version_string = true;
+          hide_key_hints = true;
+          hide_borders = true;
+        };
       };
     };
     # getty = {
