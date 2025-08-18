5706d339592cb072bb5f6731cb06bbdcbc3d45ed
chea-vuthearith <cheavuthearith0@gmail.com>
2025-08-18 18:30:39 +0700

    fix: floating steam

--- nix/hosts/desktop/home.nix
+++ nix/hosts/desktop/home.nix
@@ -7,16 +7,11 @@
     right = [ "systray" "hypridle" "volume" "network" ];
   };
 
-  wayland.windowManager.hyprland.plugins = [
-    inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
-
-  ];
-
   programs.zsh.shellAliases = {
     nsw =
       "git -C ~/dotfiles pull; sudo nixos-rebuild switch --flake ~/dotfiles/nix#desktop --impure";
-    eldenring = ''
-      WINEDLLOVERRIDES="dinput8.dll=n,b" umu-run "$HOME/Games/Elden Ring/Game/eldenring.exe"'';
+    er = ''
+      xrandr --output DP-3 --primary && WINEDLLOVERRIDES="dinput8.dll=n,b;d3dcompiler_47=n;dxgi=n,b" umu-run "$HOME/Games/Elden Ring/Game/eldenring.exe"'';
   };
 
   home.packages = with pkgs; [
@@ -24,7 +19,4 @@
     mono
     umu-launcher
   ];
-
-  wayland.windowManager.hyprland.systemd.extraCommands =
-    [ "xrandr --output DP-3 --primary" ];
 }
