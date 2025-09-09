686965a88a8210f41fd68e98665c8e372fee41b4
chea-vuthearith <cheavuthearith0@gmail.com>
2025-09-09 14:09:17 +0700

    fix: disable nvim dash

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,16 +1,7 @@
-math.randomseed(os.time() + vim.loop.hrtime())
 return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    dashboard = {
-      sections = {
-        {
-          pane = 1,
-          { section = "keys", gap = 1, padding = 1 },
-          { section = "startup" },
-        },
-      },
-    },
+    dashboard = { enabled = false },
   },
 }
