6ac269f309c0aa9c5ee14afc4f98331992cad0ca
chea-vuthearith <cheavuthearith0@gmail.com>
2026-01-25 03:52:09 +0700

    feat: remove cache keys

--- nix/hosts/desktop/home-manager/overrides/cache-client.nix
+++ nix/hosts/desktop/home-manager/overrides/cache-client.nix
@@ -1,7 +1,6 @@
 { ... }: {
   nix.settings = {
     substituters = [ "http://192.168.108.74:5000" ];
-    trusted-public-keys =
-      [ "local-nix-cache:AhF0FXOwu8HBWPpDd0S8QEptvnxMqhUTRlPIWBTLvio=" ];
+    require-sigs = false;
   };
 }
--- nix/hosts/laptop/configuration/overrides/cache-client.nix
+++ nix/hosts/laptop/configuration/overrides/cache-client.nix
@@ -1,7 +1,6 @@
 { ... }: {
   nix.settings = {
     substituters = [ "http://192.168.100.74:5000" ];
-    trusted-public-keys =
-      [ "desktop:iLOMVEpAqoXMyrqlG+PLTs3h+KI63+bzPy/vpAmkzYc=" ];
+    require-sigs = false;
   };
 }
