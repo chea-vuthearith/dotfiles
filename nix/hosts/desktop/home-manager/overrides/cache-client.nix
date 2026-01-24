38e6f19a4a6b324f9e5b7ecce85896733e302d9e
chea-vuthearith <cheavuthearith0@gmail.com>
2026-01-25 03:24:17 +0700

    feat: add public key to desktop sub

--- nix/hosts/desktop/home-manager/overrides/cache-client.nix
+++ nix/hosts/desktop/home-manager/overrides/cache-client.nix
@@ -2,6 +2,6 @@
   nix.settings = {
     substituters = [ "http://192.168.108.74:5000" ];
     trusted-public-keys =
-      [ "desktop:iLOMVEpAqoXMyrqlG+PLTs3h+KI63+bzPy/vpAmkzYc=" ];
+      [ "local-nix-cache:AhF0FXOwu8HBWPpDd0S8QEptvnxMqhUTRlPIWBTLvio=" ];
   };
 }
