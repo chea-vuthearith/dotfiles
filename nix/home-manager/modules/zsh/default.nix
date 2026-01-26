295329b5fd9f5bed4c0c80d401cae6f83085c865
chea-vuthearith <cheavuthearith0@gmail.com>
2026-01-26 09:58:23 +0700

    fix: make nsws a switch command to use cache

--- nix/home-manager/modules/zsh/default.nix
+++ nix/home-manager/modules/zsh/default.nix
@@ -11,18 +11,13 @@
       ignoreDups = true;
       findNoDups = true;
     };
-    plugins = [
-      {
-        name = "zinit";
-        src = pkgs.zinit;
-        file = "share/zinit/zinit.zsh";
-      }
-    ];
+    plugins = [{
+      name = "zinit";
+      src = pkgs.zinit;
+      file = "share/zinit/zinit.zsh";
+    }];
     shellAliases = {
-      pysrc = "source .venv/bin/activate";
-      pyenv = "py -m venv .venv && pysrc";
-      nsh = "nix-shell --run zsh";
-      ncf = "yazi ~/dotfiles/nix";
+      nsws = "nsw --option substitute true";
       wgu = "sudo systemctl start wg-quick-wg0.service";
       wgd = "sudo systemctl stop wg-quick-wg0.service";
       dl =
