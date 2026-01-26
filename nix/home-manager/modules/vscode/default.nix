d940bf9d6a13db083556a2f499a5a9d6215dd4b0
chea-vuthearith <cheavuthearith0@gmail.com>
2026-01-27 00:24:46 +0700

    fix: use symlinks for vscode confs

--- nix/home-manager/modules/vscode/default.nix
+++ nix/home-manager/modules/vscode/default.nix
@@ -1,9 +1,15 @@
 {config, ...}:{
+  home.file = {
+    ".config/Code/User/settings.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/dotfiles/nix/home-manager/modules/vscode/settings.json";
+    };
+    ".config/Code/User/keybindings.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/dotfiles/nix/home-manager/modules/vscode/keybindings.json";
+    };
+  };
   programs.vscode = {
     enable = true;
-    profiles.${config.home.userName} = {
-      userSettings = builtins.readFile ./settings.json;
-      keybindings = builtins.readFile ./keybindings.json;
-    };
   };
 }
