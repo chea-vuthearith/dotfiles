596a50f3c4e3496ad4efd07965fb82690052e58e
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-01 22:26:34 +0700

    fix: keep wallpaper daemon alive

--- nix/home-manager/modules/wallpaper.nix
+++ nix/home-manager/modules/wallpaper.nix
@@ -2,6 +2,18 @@
 let awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww;
 in {
   home.packages = [ awww ];
+  systemd.user.services.awww-daemon = {
+    Unit = {
+      Description = "Awww Wallpaper Daemon";
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${awww}/bin/awww-daemon";
+      Restart = "always";
+    };
+    Install = { WantedBy = [ "default.target" ]; };
+  };
+
   home.activation.awwwWallpaper = ''
     if ! pgrep -x awww-daemon > /dev/null; then
       (${awww}/bin/awww-daemon &)
