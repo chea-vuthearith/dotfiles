a386b9c0922536836944a79475125b4bbb07514d
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-04 00:05:11 +0700

    feat: configured nixd

--- home-manager/modules/nvim/conf/lua/plugins/nvim-lspconfig.lua
+++ home-manager/modules/nvim/conf/lua/plugins/nvim-lspconfig.lua
@@ -75,10 +75,10 @@ return {
             },
             options = {
               nixos = {
-                expr = '(builtins.getFlake "github:chea-vuthearith/nix").nixosConfigurations.desktop.options',
+                expr = '(builtins.getFlake "github:chea-vuthearith/dotfiles").nixosConfigurations.desktop.options',
               },
               home_manager = {
-                expr = '(builtins.getFlake "github:chea-vuthearith/nix").nixosConfigurations.desktop.options.home-manager.users.type.getSubOptions []',
+                expr = '(builtins.getFlake "github:chea-vuthearith/dotfiles").nixosConfigurations.desktop.options.home-manager.users.type.getSubOptions []',
               },
             },
           },
