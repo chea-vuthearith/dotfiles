cfed0cb55e1556f79681a4a93c6deda1e2b2e796
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-05 23:12:17 +0700

    feat: disable mason and manage binaries through nix

--- home-manager/modules/nvim/default.nix
+++ home-manager/modules/nvim/default.nix
@@ -4,8 +4,56 @@
   inputs,
   ...
 }: {
-  home.packages = with pkgs; [neovim nixd alejandra statix];
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+  home.packages = with pkgs; [
+    # General/Editor
+    neovim
+
+    # Nix
+    nixd
+    alejandra
+    statix
+
+    # Python
+    pyright
+    ruff
+
+    # JavaScript/TypeScript
+    vtsls
+    biome
+
+    # Lua
+    lua-language-server
+    stylua
+
+    # Markdown
+    markdown-toc
+    markdownlint-cli2
+    marksman
+
+    # Shell/Bash
+    bash-language-server
+    shellcheck
+    shfmt
+
+    # Docker
+    docker-compose-language-service
+    dockerfile-language-server
+    hadolint
+
+    # SQL
+    sqlfluff
+    prisma-language-server
+
+    # CSS/HTML/Frontend
+    tailwindcss-language-server
+
+    # Tree-sitter
+    tree-sitter
+
+    # YAML
+    yaml-language-server
+  ];
+  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"]; # for nixd lsp
   programs.zsh.shellAliases = {vi = "nvim";};
   home.file = {
     ".config/nvim" = {
