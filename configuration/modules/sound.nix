02d63a47b8cbe4798d3c458b8e30a698617e62b4
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-07 00:25:59 +0700

    chore: remove redundant config

--- configuration/modules/sound.nix
+++ configuration/modules/sound.nix
@@ -1,9 +1,5 @@
 {...}: {
-  hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
   };
 }
