890279b306a5d6cdcc684da704feddc6965f908f
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-08 00:04:53 +0700

    feat: enabled bufferline

--- home-manager/modules/nvim/conf/lua/plugins/disabled.lua
+++ home-manager/modules/nvim/conf/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "nvim.mini/mini.pairs", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-ts-autotag", enabled = false },
   { "mason-org/mason.nvim", enabled = false },
   { "mason-org/mason-lspconfig.nvim", enabled = false },
