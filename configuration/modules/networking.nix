2d37d870a385d210b5e393a7e0b00b68c86f0065
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-09 00:57:33 +0700

    fix: reset network settings

--- configuration/modules/networking.nix
+++ configuration/modules/networking.nix
@@ -3,7 +3,6 @@
     hostName = "nixos";
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd"; # faster than wpa_supplicant
     };
     firewall = {
       enable = true;
@@ -17,23 +16,4 @@
   };
 
   systemd.network.wait-online.enable = false; # network dont block boot
-
-  services.resolved = {
-    enable = true;
-    settings.Resolve = {
-      DNSSEC = "false";
-      FallbackDNS = ["1.1.1.1" "1.0.0.1"];
-    };
-  };
-
-  boot = {
-    # faster wifi
-    kernelModules = ["tcp_bbr"];
-    kernel.sysctl = {
-      # network performance
-      "net.core.netdev_max_backlog" = 16384;
-      "net.ipv4.tcp_fastopen" = 3;
-      "net.ipv4.tcp_congestion_control" = "bbr";
-    };
-  };
 }
