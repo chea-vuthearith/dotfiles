2fb7922ad98162998c6933649ea744e775f1dfe4
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-09 01:45:19 +0700

    feat: disable sudo for nh

--- hosts/desktop/home-manager/overrides/zsh.nix
+++ hosts/desktop/home-manager/overrides/zsh.nix
@@ -1,6 +1,6 @@
 {...}: {
   programs.zsh.shellAliases = {
-    nsw = "git -C ~/dotfiles pull; sudo nh os switch ~/dotfiles -H=desktop --impure";
+    nsw = "git -C ~/dotfiles pull; nh os switch ~/dotfiles -H=desktop --impure";
     nsws = "nsw -- --option substituters 'http://192.168.100.108:5000' --option require-sigs false";
   };
 }
