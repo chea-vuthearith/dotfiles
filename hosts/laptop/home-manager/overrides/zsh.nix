2b2e0c438d1079676e28df9d656780d7bf989cce
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-09 01:49:43 +0700

    Reapply "feat: disable sudo for nh"
    This reverts commit b1d06bf225f1e467682b6a56d218b08f2f532685.

--- hosts/laptop/home-manager/overrides/zsh.nix
+++ hosts/laptop/home-manager/overrides/zsh.nix
@@ -1,6 +1,6 @@
 {...}: {
   programs.zsh.shellAliases = {
-    nsw = "git -C ~/dotfiles pull; sudo nh os switch ~/dotfiles -H=laptop --impure";
+    nsw = "git -C ~/dotfiles pull; nh os switch ~/dotfiles -H=laptop --impure";
     nsws = "nsw -- --option substituters 'http://192.168.100.74:5000' --option require-sigs false";
   };
 }
