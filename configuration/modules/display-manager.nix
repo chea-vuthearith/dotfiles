922aec4b2520f774aa1f594fc2c3441a3047fdac
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-09 02:10:19 +0700

    feat: add mini-hipatterns

--- configuration/modules/display-manager.nix
+++ configuration/modules/display-manager.nix
@@ -1,11 +1,9 @@
-{config, ...}: {
+{...}: {
   services.displayManager = {
     enable = true;
     ly = {
       enable = true;
-      settings = with config.lib.stylix.colors; {
-        fg = "0xFF${base05}";
-        error_fg = "0xFF${base08}";
+      settings = {
         hide_version_string = true;
         hide_key_hints = true;
         hide_borders = true;
