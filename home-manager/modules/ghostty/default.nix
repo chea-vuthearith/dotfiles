cb58653a8944878ad68df58005abdf8b16c86eca
chea-vuthearith <cheavuthearith0@gmail.com>
2026-02-21 18:33:26 +0700

    chore: disable close prompt

--- home-manager/modules/ghostty/default.nix
+++ home-manager/modules/ghostty/default.nix
@@ -1,10 +1,13 @@
-{...}: {
-  home.sessionVariables.TERMCMD = "ghostty -e";
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    ueberzugpp
+  ];
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
     systemd.enable = true;
     settings = {
+      confirm-close-surface = false;
       mouse-hide-while-typing = true;
       resize-overlay = "never";
     };
