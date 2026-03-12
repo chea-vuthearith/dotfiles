46522322eeb4da9c3018626d6f9df8b007bc535e
chea-vuthearith <cheavuthearith0@gmail.com>
2026-03-12 09:09:26 +0700

    styling: reduce ghostty padding
    y padding no longer needed since tmux bar is on top

--- home-manager/modules/ghostty/default.nix
+++ home-manager/modules/ghostty/default.nix
@@ -11,8 +11,8 @@
     enableZshIntegration = true;
     systemd.enable = true;
     settings = {
-      window-padding-x = "10";
-      window-padding-y = "10,0";
+      window-padding-x = "5";
+      window-padding-y = "0";
       shell-integration-features = "ssh-env, title";
       font-family = config.stylix.fonts.monospace.name;
       font-family-italic = "VictorMono Nerd Font";
