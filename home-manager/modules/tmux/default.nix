1bb7f30073993d0402f6c21aad2af155de96befc
chea-vuthearith <cheavuthearith0@gmail.com>
2026-03-20 21:53:15 +0700

    feat: use full sesh in normal term

--- home-manager/modules/tmux/default.nix
+++ home-manager/modules/tmux/default.nix
@@ -44,11 +44,25 @@ in {
         sc = "sesh connect";
       };
       initContent = lib.mkOrder 1500 ''
+        __sesh_fzf() {
+          sesh list --icons | fzf-tmux -p 80%,70% \
+            --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
+            --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+            --bind 'tab:down,btab:up' \
+            --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+            --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+            --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
+            --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+            --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+            --preview-window 'right:55%' \
+            --preview 'sesh preview {}'
+        }
         __sesh_prefix() {
           local key
           read -k key
           case $key in
-            s) BUFFER='s'; zle accept-line ;;
+            s) BUFFER="sesh connect $(__sesh_fzf)"; zle accept-line ;;
             .) BUFFER='sesh connect .'; zle accept-line ;;
             *) zle -U "w$key" ;;
           esac
