99cb8ae9173a11e82e845e917eaaf57f44125b83
chea-vuthearith <cheavuthearith0@gmail.com>
2026-03-21 14:59:51 +0700

    fix: keybinds

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -15,7 +15,6 @@
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     plugins = [
       inputs.split-monitor-workspaces.packages.${pkgs.stdenv.hostPlatform.system}.split-monitor-workspaces
-      pkgs.hyprlandPlugins.hypr-dynamic-cursors
     ];
     extraConfig = "
       source=${./hypr/general.conf}
