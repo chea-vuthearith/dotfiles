be4861b906eb97d4a783dc88c81b900ad2e34d0b
chea-vuthearith <cheavuthearith0@gmail.com>
2026-03-21 15:51:32 +0700

    fix: flake targets

--- hosts/desktop/home-manager/overrides/zsh.nix
+++ hosts/desktop/home-manager/overrides/zsh.nix
@@ -1,6 +1,6 @@
 {...}: {
   programs.zsh.shellAliases = {
-    nsw = "git -C ~/dotfiles pull; sudo nixos-rebuild switch --flake ~/dotfiles#laptop --impure";
+    nsw = "git -C ~/dotfiles pull; sudo nixos-rebuild switch --flake ~/dotfiles#desktop --impure";
     nsws = "nsw --option substituters 'http://192.168.100.108:5000' --option trusted-substituters 'http://192.168.100.108:5000' --option require-sigs false";
   };
 }
