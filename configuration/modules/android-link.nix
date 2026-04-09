7c493c5502ee36f8c43f9a2761a7792be3afacd2
chea-vuthearith <cheavuthearith0@gmail.com>
2026-04-09 18:23:56 +0700

    fix: firewall rules for kde con

---
+++ configuration/modules/android-link.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}: {
+  programs.kdeconnect = {
+    enable = true;
+    package = pkgs.valent;
+  };
+
+  networking.firewall = {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+  };
+}
