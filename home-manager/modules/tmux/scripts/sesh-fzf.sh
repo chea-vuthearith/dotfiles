bd7c094b7882d68bb014a166e90844f8c1148574
chea-vuthearith <cheavuthearith0@gmail.com>
2026-05-13 12:01:28 +0700

    chore: only show noti on kill fail

--- home-manager/modules/tmux/scripts/sesh-fzf.sh
+++ home-manager/modules/tmux/scripts/sesh-fzf.sh
@@ -3,20 +3,16 @@ sesh list --icons | fzf-tmux -p 80%,70% \
   --ansi \
   --header "  ^a all ^t tmux ^x zoxide ^d kill" \
   --bind 'ctrl-a:reload(sesh list --icons)' \
-  --bind 'ctrl-t:change-prompt(  )+reload(sesh list -t --icons)' \
+  --bind 'ctrl-t:change-prompt(  )+reload(sesh list -t --icons)' \
   --bind 'ctrl-x:change-prompt(󰈞  )+reload(sesh list -z --icons)' \
   --bind 'ctrl-d:reload(
     name={2..}
     if sesh list -T | grep -qxF "$name"; then
-      if output=$(tmuxinator stop "$name" 2>&1); then
-        notify-send "Tmux" "Session stopped: $name"
-      else
-        notify-send -u critical "Tmux" "Failed to stop session: $name\n$output"
+      if ! output=$(tmuxinator stop "$name" 2>&1); then
+        notify-send -u critical "Tmux" "Failed to kill session: $name\n$output"
       fi
     else
-      if output=$(tmux kill-session -t "$name" 2>&1); then
-        notify-send "Tmux" "Session killed: $name"
-      else
+      if ! output=$(tmux kill-session -t "$name" 2>&1); then
         notify-send -u critical "Tmux" "Failed to kill session: $name\n$output"
       fi
     fi
