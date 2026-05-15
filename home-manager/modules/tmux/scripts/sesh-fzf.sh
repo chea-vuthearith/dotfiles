d7ef6805b5221b074757e5695675bcb4d3d101d5
chea-vuthearith <cheavuthearith0@gmail.com>
2026-05-15 17:18:42 +0700

    fix: tmuxinator proj blocking stop

--- home-manager/modules/tmux/scripts/sesh-fzf.sh
+++ home-manager/modules/tmux/scripts/sesh-fzf.sh
@@ -5,18 +5,26 @@ sesh list --icons | fzf-tmux -p 80%,70% \
   --bind 'ctrl-a:reload(sesh list --icons)' \
   --bind 'ctrl-t:change-prompt(  )+reload(sesh list -t --icons)' \
   --bind 'ctrl-x:change-prompt(󰈞  )+reload(sesh list -z --icons)' \
-  --bind 'ctrl-d:reload(
-    name={2..}
+  --bind 'ctrl-d:execute-silent(
+    name={2..};
+
     if sesh list -T | grep -qxF "$name"; then
-      if ! output=$(tmuxinator stop "$name" 2>&1); then
-        notify-send -u critical "Tmux" "Failed to kill session: $name\n$output"
-      fi
+      notify-send "Tmuxinator" "Stopping $name"
+
+      nohup sh -c '\''
+
+        if output=$(tmuxinator stop "$1" 2>&1); then
+          notify-send "Tmux" "Stopped tmuxinator project: $1"
+        else
+          notify-send -u critical "Tmuxinator" "Failed to stop $1 \n$output"
+        fi
+
+      '\'' sh "$name" >/dev/null 2>&1 &
     else
       if ! output=$(tmux kill-session -t "$name" 2>&1); then
         notify-send -u critical "Tmux" "Failed to kill session: $name\n$output"
       fi
     fi
-    sesh list --icons
-  )' \
+  )+reload(sesh list --icons)' \
   --preview-window "right:55%" \
   --preview "sesh preview {}"
