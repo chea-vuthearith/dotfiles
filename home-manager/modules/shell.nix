7361b06c1fc43a3713eaded693886ab32bb2af75
chea-vuthearith <cheavuthearith0@gmail.com>
2026-06-15 21:30:15 +0700

    fix: inf recursion config

--- home-manager/modules/shell.nix
+++ home-manager/modules/shell.nix
@@ -46,15 +46,15 @@ in {
       };
     };
 
-    settings = {
+    settings = rec {
       acLockTimeout = 900;
       acMonitorTimeout = 600;
       acPostLockMonitorTimeout = 600;
       acSuspendTimeout = 3600;
-      batteryLockTimeout = config.programs.dank-material-shell.settings.acLockTimeout;
-      batteryMonitorTimeout = config.programs.dank-material-shell.settings.acMonitorTimeout;
-      batteryPostLockMonitorTimeout = config.programs.dank-material-shell.settings.acPostLockMonitorTimeout;
-      batterySuspendTimeout = config.programs.dank-material-shell.settings.acSuspendTimeout;
+      batteryLockTimeout = acLockTimeout;
+      batteryMonitorTimeout = acMonitorTimeout;
+      batteryPostLockMonitorTimeout = acPostLockMonitorTimeout;
+      batterySuspendTimeout = acSuspendTimeout;
 
       builtInPluginSettings = {
         dms_settings_search = {
